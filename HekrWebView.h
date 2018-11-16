b9d371979a038f393fe2bd250d20e9655765ec0d
wenbo.ye <>
2018-11-16 11:55:36 +0800

    增加自定义的注册func的方法

--- HekrWebView.h
+++ HekrWebView.h
@@ -13,13 +13,15 @@
 -(void)viewBodyUpdate;
 -(void)hekrWebViewDidFail;
 -(void)hekrWebViewDidFinish;
+
+@required
 /**
  需要vc自己处理的事件
 
- @param callFunc [setStateBarColor,screenShot,close]
+ @param funcName [setStateBarColor(对于web没有把状态栏也覆盖的控制界面，需要自己维护状态栏的背景颜色),screenShot(截屏),close(pop返回上个界面)]
  @param body body
  */
--(void)callFunc:(NSString *)callFunc body:(id)body;
+-(void)callFunc:(NSString *)funcName body:(id)body;
 
 @end
 
@@ -38,7 +40,28 @@
 -(void)reload;
 -(void)destory;
 
+/**
+ 自定义注入的js，格式请严格按照SDK内的HekrWebJSBridge.js
+ 
+ @param js js
+ */
 - (void) addCustomJavaScript:(NSString *)js;
+
+/**
+ 事件的返回
+ 
+ @param event event
+ @param data data
+ */
 -(void) evaluateJavaScript:(NSString *)event data:(NSDictionary *)data;
+
+/**
+ 对addCustomJavaScript的自定义的桥接注册方法名
+ 需要在startLoad前注册
+ 
+ @param funcName funcName
+ @param imp imp 对于非事件处理的方法名，callback必须返回参数，否则就会闪退
+ */
+-(void)registerFunc:(NSString *)funcName imp:(void(^)(HekrWebView *_,id body,void(^callback)(NSString *))) imp;
 
 @end
